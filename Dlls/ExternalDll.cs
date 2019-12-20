5ced8df254229309dc004e440a4f4abaab490434
xuweijinjin <44576006+xuweijinjin@users.noreply.github.com>
2019-12-20 19:04:26 +0800

    Add files via upload

---
+++ Dlls/ExternalDll.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JinVoice.Dlls
+{
+    public static class ExternalDll
+    {
+        [DllImport("Kernel32.dll")] //引入命名空间 using System.Runtime.InteropServices;  
+        public static extern bool Beep(int frequency, int duration);// 第一个参数是指频率的高低，越大越高，第二个参数是指响的时间多
+
+    }
+}
